049515ce8b150b42189d96c645fd25511d038469
ChowdhuryNurul <nurul_chowdhury@hotmail.com>
2022-08-03 22:38:25 -0400

    Mini Project: Madlib

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+Number_States = input("How Many States? : ") #50
+City = input ("City Name: ") #New York
+Capital = input ("What is the Capital of USA? : ") # Washington, DC
+City2 = input("Famous for Hollywood Filmed making : ") #Los Angeles
+madlib = f"The U.S. is a country of {Number_States} states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. \
+ Major Atlantic Coast cities are {City}, a global finance and culture center, and capital {Capital}.Midwestern metropolis Chicago is known for influential architecture and on the west coast, {City2}' Hollywood is famed for filmmaking."
+
+print(madlib)
